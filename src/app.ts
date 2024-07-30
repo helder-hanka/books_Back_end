c3de6cae7ebedc98308bc6140832f7c7e9e05fc6
helder-hanka <hfernandes238@gmail.com>
2024-07-30 23:56:34 +0200

    Config app express

--- src/app.ts
+++ src/app.ts
@@ -15,6 +15,39 @@ mongoose
 const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
+app.use("/api", book);
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+export default app;
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import book from "./routes/book";
+
+dotenv.config();
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose
+  .connect(`${MONGODB_URI}`)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Could not connect to MongoDB", err));
+
+const app = express();
+app.use(express.json());
+app.use(bodyParser.json());
 app.use("/api/books", book);
 
 app.use((req, res, next) => {
