84c7de06325805b402f388ca6ba78e3de78d2d4a
helder-hanka <hfernandes238@gmail.com>
2024-07-31 00:05:53 +0200

    Define Book modéle with TS

--- src/models/Book.ts
+++ src/models/Book.ts
@@ -1,14 +1,22 @@
 import { Schema, model, Document } from "mongoose";
 
+<<<<<<< HEAD
 // interface IRating {
 //   userId: Schema.Types.ObjectId;
 //   grade: number;
 // }
+=======
+interface IRating {
+  userId: Schema.Types.ObjectId;
+  grade: number;
+}
+>>>>>>> b4869e9 (Define Book modéle with TS)
 
 export interface IBook extends Document {
   title: string;
   author: string;
   imageUrl: string;
+<<<<<<< HEAD
   year: number;
   genre: string;
   // ratings: IRating[];
@@ -59,5 +67,61 @@ const BookSchema = new Schema<IBook>(
   },
   { timestamps: true }
 );
+=======
+  genre: string;
+  ratings: IRating[];
+  averageRating: number;
+  User: Schema.Types.ObjectId;
+}
+
+const ratingSchema = new Schema<IRating>({
+  userId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+  grade: {
+    type: Number,
+    required: true,
+  },
+});
+
+const BookSchema = new Schema<IBook>({
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  genre: {
+    type: String,
+    required: true,
+  },
+  ratings: {
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+    grade: {
+      type: Number,
+      required: true,
+    },
+  },
+  averageRating: {
+    type: Number,
+    required: true,
+  },
+  User: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+});
+>>>>>>> b4869e9 (Define Book modéle with TS)
 
 export default model<IBook>("Book", BookSchema);
