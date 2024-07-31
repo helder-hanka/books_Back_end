80687c0ce7f2bfe0234dfbd15033895cdde7c4e7
helder-hanka <hfernandes238@gmail.com>
2024-07-31 22:50:49 +0200

    add createBook func post

--- src/controller/book.ts
+++ src/controller/book.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import Book from "../models/Book";
 
 export const getBooks = (req: Request, res: Response) => {
   console.log("ok");
@@ -6,5 +7,16 @@ export const getBooks = (req: Request, res: Response) => {
     res.status(200).json({ message: "ok" });
   } catch (error) {
     res.status(500).json({ error: "error" });
+  }
+};
+
+export const createBook = async (req: Request, res: Response) => {
+  const Uid = "66aa5fcfbb18321a4e028fcd";
+  try {
+    const book = new Book({ ...req.body, UserId: Uid });
+    const a = await book.save();
+    res.status(201).json(a);
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
   }
 };
