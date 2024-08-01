ecdd6738f654138e59d482795764800d0055cd1b
helder-hanka <hfernandes238@gmail.com>
2024-08-01 12:24:27 +0200

    Add a Check emil alredy existing

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -4,10 +4,16 @@ import User from "../models/User";
 
 export const signup = async (req: Request, res: Response) => {
   try {
+    const existingUser = await User.findOne({ email: req.body.email });
+
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already in use" });
+    }
+
     const passwordHash = await bcrypt.hash(req.body.password, 10);
 
     const user = new User({ ...req.body, password: passwordHash });
-    user.save();
+    await user.save();
 
     res.status(201).json({ message: "User created" });
   } catch (error) {
