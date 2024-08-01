3ecb8a88665947c626bf37b93ab6999ff9b7a955
helder-hanka <hfernandes238@gmail.com>
2024-08-01 10:50:34 +0200

    Resolve conflict

--- src/controller/book.ts
+++ src/controller/book.ts
@@ -1,8 +1,5 @@
 import { Request, Response } from "express";
-<<<<<<< HEAD
 import Book from "../models/Book";
-=======
->>>>>>> 817e6a9 (Define operation getBooks func)
 
 export const getBooks = (req: Request, res: Response) => {
   console.log("ok");
@@ -12,7 +9,6 @@ export const getBooks = (req: Request, res: Response) => {
     res.status(500).json({ error: "error" });
   }
 };
-<<<<<<< HEAD
 
 export const createBook = async (req: Request, res: Response) => {
   const Uid = "66aa5fcfbb18321a4e028fcd";
@@ -24,5 +20,3 @@ export const createBook = async (req: Request, res: Response) => {
     res.status(500).json({ error: error.message });
   }
 };
-=======
->>>>>>> 817e6a9 (Define operation getBooks func)
