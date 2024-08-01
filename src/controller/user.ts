28cdcde0e9b55e11f7fde7f257aea44abc009dd6
helder-hanka <hfernandes238@gmail.com>
2024-08-01 12:25:00 +0200

    i jwt

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -20,3 +20,16 @@ export const signup = async (req: Request, res: Response) => {
     res.status(500).json({ error });
   }
 };
+
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  const err = "Incorrect email or password !";
+  try {
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      res.status(401).json({ message: err });
+    }
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
