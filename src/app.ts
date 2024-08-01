983a8bc296638d8faca13c2a5a7ea6c8bf0a1aa2
helder-hanka <hfernandes238@gmail.com>
2024-08-01 11:46:29 +0200

    add / in router user

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
 app.use("/api/books", book);
-app.use("api/auth", user);
+app.use("/api/auth", user);
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
