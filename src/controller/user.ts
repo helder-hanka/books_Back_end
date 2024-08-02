d67113f0cdee4764edf47fbbb61970ec0ef2cd5f
helder-hanka <hfernandes238@gmail.com>
2024-08-02 05:37:56 +0200

    Update login func

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import bcrypt from "bcrypt";
 import User from "../models/User";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req: Request, res: Response) => {
   try {
@@ -29,7 +30,29 @@ export const login = async (req: Request, res: Response) => {
     if (!user) {
       res.status(401).json({ message: err });
     }
+    if (!user?.password) {
+      return res.status(500).json({ message: "Internal server error" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: err });
+    }
+
+    const SECRET_KEY = process.env.SECRET_KEY;
+    if (!SECRET_KEY) {
+      return res.status(401).json({ message: "SECRET_KEY is not defined" });
+    }
+
+    const token = jwt.sign(
+      { email: user.email, userId: user._id },
+      SECRET_KEY,
+      { expiresIn: "24h" }
+    );
+    res
+      .status(200)
+      .json({ message: "Login successful", token: token, userId: user._id });
   } catch (error) {
-    res.status(500).json({ error });
+    res.status(500).json({ message: error });
   }
 };
