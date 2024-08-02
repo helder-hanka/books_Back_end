7a1266d99686b0a3323a06bdc197b5f8426a53fa
helder-hanka <hfernandes238@gmail.com>
2024-08-02 06:51:07 +0200

    Import middleware auth, add in route createBook pos

--- src/routes/book.ts
+++ src/routes/book.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { getBooks } from "../controller/book";
 import { createBook } from "../controller/book";
+import { auth } from "../middleware/auth";
 
 const router = Router();
 
 router.get("/", getBooks);
-router.post("/", createBook);
+router.post("/", auth, createBook);
 
 export default router;
