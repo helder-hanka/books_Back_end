ef9a0ffa8b50de7adb050a70a4f478dce705d408
helder-hanka <hfernandes238@gmail.com>
2024-08-09 23:25:05 +0100

    Create route get book by id

--- src/routes/book.ts
+++ src/routes/book.ts
@@ -4,6 +4,7 @@ import {
   createBook,
   updateBook,
   deleteBook,
+  getBooksById,
 } from "../controller/book";
 import { auth } from "../middleware/auth";
 import { uploadImg } from "../middleware/multer-config";
@@ -11,6 +12,7 @@ import { uploadImg } from "../middleware/multer-config";
 const router = Router();
 
 router.get("/", getBooks);
+router.get("/:id", getBooksById);
 router.post("/", auth, uploadImg, createBook);
 router.put("/:id", auth, uploadImg, updateBook);
 router.delete("/:id", auth, deleteBook);
