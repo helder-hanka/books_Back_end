735b1611d7c8eb7a2e70b2b52d4d61b9cb04e0fe
helder-hanka <hfernandes238@gmail.com>
2024-08-09 22:19:22 +0100

    Create updatedBook func and import clearImg func

--- src/controller/book.ts
+++ src/controller/book.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import Book from "../models/Book";
+import { clearImg } from "../util/fs";
 
 export const getBooks = async (req: Request, res: Response) => {
   try {
@@ -27,5 +28,43 @@ export const createBook = async (req: Request, res: Response) => {
     res.status(201).json({ message: "Product created successfully!" });
   } catch (error: any) {
     res.status(500).json({ error: error.message });
+  }
+};
+
+export const updateBook = async (req: Request, res: Response) => {
+  const { params } = req.body;
+  const paramId = req.params.id;
+  const uid = req.userId;
+  const imgFile = req.file?.filename;
+  let imageUrl = req.body.imageUrl;
+  try {
+    if (imgFile) {
+      imageUrl = `${req.protocol}://${req.get("host")}/images/${imgFile}`;
+    }
+    if (!imageUrl) {
+      throw new Error("No image provided");
+    }
+
+    const book = await Book.findById(paramId);
+
+    if (book?.UserId != uid) {
+      throw new Error("Not authorized");
+    }
+
+    if (!book) {
+      throw new Error("Product not found");
+    }
+
+    if (imgFile) {
+      clearImg(`images/${book.imageUrl.split("/images/")[1]}`);
+    }
+
+    await Book.findByIdAndUpdate(paramId, {
+      ...req.body,
+      imageUrl: imageUrl,
+    });
+    res.status(200).json({ message: "Product updated" });
+  } catch (error) {
+    res.status(500).json(error);
   }
 };
