bb704b62534e6be6f274bcdafe0382ae652794ae
helder-hanka <hfernandes238@gmail.com>
2024-08-10 04:02:59 +0100

    Created bestrating router

--- src/routes/book.ts
+++ src/routes/book.ts
@@ -6,6 +6,7 @@ import {
   deleteBook,
   getBooksById,
   averageRating,
+  bestrating,
 } from "../controller/book";
 import { auth } from "../middleware/auth";
 import { uploadImg } from "../middleware/multer-config";
@@ -14,9 +15,10 @@ const router = Router();
 
 router.get("/", getBooks);
 router.get("/:id", getBooksById);
+router.get("/bestrating", bestrating);
 router.post("/", auth, uploadImg, createBook);
+router.post("/:id/rating", auth, averageRating);
 router.put("/:id", auth, uploadImg, updateBook);
 router.delete("/:id", auth, deleteBook);
-router.post("/:id/rating", auth, averageRating);
 
 export default router;
